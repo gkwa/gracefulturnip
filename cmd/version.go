0226edab1d8de9764b13203cfa8ce077ef1cdb35
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-14 15:40:23 -0700

    cleanup boilerplate

--- cmd/version.go
+++ cmd/version.go
@@ -7,16 +7,9 @@ import (
 	"github.com/taylormonacelli/gracefulturnip/version"
 )
 
-// versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Show verision info",
 	Run: func(cmd *cobra.Command, args []string) {
 		buildInfo := version.GetBuildInfo()
 		fmt.Println(buildInfo)
