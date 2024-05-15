6681245600d2309dce80eb12fcb23f5251b8a80b
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-14 17:03:08 -0700

    keep from re-running vet, fmt, tidy if source files haven't changed

--- magefile.go
+++ magefile.go
@@ -24,7 +24,7 @@ var (
 	fullGitSHA  = ""
 )
 
-var Default = Iterate // default mage target
+var Default = Iterate
 
 const (
 	ldFlagsPrefix = "main"
@@ -34,29 +34,22 @@ const (
 func init() {
 	var err error
 	date = time.Now().UTC().Format(time.RFC3339)
-
 	r, err := git.PlainOpen(".")
 	CheckIfError(err)
-
 	ref, err := r.Head()
 	CheckIfError(err)
-
 	cIter, err := r.Log(&git.LogOptions{From: ref.Hash()})
 	CheckIfError(err)
-
 	commit, err := cIter.Next()
 	CheckIfError(err)
-
 	version = commit.Hash.String()[:8]
 	if commit.NumParents() > 0 {
 		version += "-dirty"
 	}
-
 	goVersion, err = sh.Output("go", "version")
 	if err != nil {
 		fmt.Printf("Error getting Go version: %v\n", err)
 	}
-
 	shortGitSHA = commit.Hash.String()[:7]
 	fullGitSHA = commit.Hash.String()
 }
@@ -66,15 +59,87 @@ func Iterate() {
 }
 
 func Lint() error {
-	return sh.Run("golangci-lint", "run")
+	sources, err := getSourceFiles(".")
+	if err != nil {
+		return err
+	}
+
+	lintTime := time.Time{}
+	if _, err := os.Stat(".lint.time"); err == nil {
+		lintTime, err = target.NewestModTime(".lint.time")
+		if err != nil {
+			return err
+		}
+	}
+
+	if rebuild, err := target.DirNewer(lintTime, sources...); err != nil || !rebuild {
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := sh.Run("golangci-lint", "run"); err != nil {
+		return err
+	}
+
+	return touch(".lint.time")
 }
 
 func Fmt() error {
-	return sh.Run("gofumpt", "-w", ".")
+	sources, err := getSourceFiles(".")
+	if err != nil {
+		return err
+	}
+
+	fmtTime := time.Time{}
+	if _, err := os.Stat(".fmt.time"); err == nil {
+		fmtTime, err = target.NewestModTime(".fmt.time")
+		if err != nil {
+			return err
+		}
+	}
+
+	if rebuild, err := target.DirNewer(fmtTime, sources...); err != nil || !rebuild {
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := sh.Run("gofumpt", "-w", "."); err != nil {
+		return err
+	}
+
+	return touch(".fmt.time")
 }
 
 func Vet() error {
-	return sh.Run("go", "vet", "./...")
+	sources, err := getSourceFiles(".")
+	if err != nil {
+		return err
+	}
+
+	vetTime := time.Time{}
+	if _, err := os.Stat(".vet.time"); err == nil {
+		vetTime, err = target.NewestModTime(".vet.time")
+		if err != nil {
+			return err
+		}
+	}
+
+	if rebuild, err := target.DirNewer(vetTime, sources...); err != nil || !rebuild {
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := sh.Run("go", "vet", "./..."); err != nil {
+		return err
+	}
+
+	return touch(".vet.time")
 }
 
 func Check() {
@@ -82,12 +147,30 @@ func Check() {
 }
 
 func Tidy() error {
-	return sh.Run("go", "mod", "tidy")
+	tidyTime := time.Time{}
+	if _, err := os.Stat(".tidy.time"); err == nil {
+		tidyTime, err = target.NewestModTime(".tidy.time")
+		if err != nil {
+			return err
+		}
+	}
+
+	if rebuild, err := target.DirNewer(tidyTime, "go.mod", "go.sum"); err != nil || !rebuild {
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := sh.Run("go", "mod", "tidy"); err != nil {
+		return err
+	}
+
+	return touch(".tidy.time")
 }
 
 func Build() error {
 	mg.Deps(Tidy)
-
 	buildTime := time.Time{}
 	if _, err := os.Stat(buildTarget); err == nil {
 		buildTime, err = target.NewestModTime(buildTarget)
@@ -95,20 +178,17 @@ func Build() error {
 			return err
 		}
 	}
-
 	sources, err := getSourceFiles(".")
 	if err != nil {
 		return err
 	}
 	sources = append(sources, "go.mod", "go.sum")
-
 	if rebuild, err := target.DirNewer(buildTime, sources...); err != nil || !rebuild {
 		if err != nil {
 			return err
 		}
 		return nil
 	}
-
 	ldflags := fmt.Sprintf(`-s -w -X '%s.Version=%s' -X '%s.Date=%s' -X '%s.GoVersion=%s' -X '%s.ShortGitSHA=%s' -X '%s.FullGitSHA=%s'`,
 		ldFlagsPrefix, version,
 		ldFlagsPrefix, date,
@@ -116,7 +196,6 @@ func Build() error {
 		ldFlagsPrefix, shortGitSHA,
 		ldFlagsPrefix, fullGitSHA,
 	)
-
 	return sh.Run("go", "build", "-ldflags", ldflags, "-o", buildTarget)
 }
 
@@ -155,4 +234,12 @@ func getSourceFiles(dir string, exts ...string) ([]string, error) {
 		return nil, err
 	}
 	return sources, nil
+}
+
+func touch(file string) error {
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	return f.Close()
 }
